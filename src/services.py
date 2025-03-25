5a0103e88412965d290ccc1f813b152ee0a19332
Deezinn <developer.andrelz@gmail.com>
2025-03-24 23:01:50 -0300

    aplicar validações futuramente

--- src/services.py
+++ src/services.py
@@ -17,14 +17,17 @@ class AgendaService:
         # aplicar validações futuramente
 
     def editar_contato(self, nome, novo_nome=None, novo_telefone=None, novo_email=None, novo_update=None):
-        #aplicar validações futuramente
+        # aplicar validações futuramente
        return self.model.editar_contato(nome, novo_nome, novo_telefone, novo_email, novo_update)
 
     def marcar_contato_favorito(self,nome, favorito):
+        # aplicar validações futuramente
         return self.model.marcar_contato_favorito(nome,favorito)
 
     def listar_contatos_favoritos(self):
+        # aplicar validações futuramente
         return self.model.listar_contatos_favoritos()
 
     def deletar_contato(self,nome):
+        # aplicar validações futuramente
         return self.model.deletar_contato(nome)
